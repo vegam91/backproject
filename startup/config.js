bb40fcd50318cfa4618c06c16d6428d1bd1ea1b8
vegam91 <vegamartinoj@gmail.com>
2023-07-25 08:03:30 +0200

    thunder enviroment

---
+++ startup/config.js
@@ -0,0 +1,11 @@
+// const config = require('config')
+
+
+// module.exports = function () {
+// 	if (!config.has('jwtSecret')) {
+// 		const msg = 'Se necesita clave privada para jwt'
+
+// 		// winston.error(msg)
+// 		// throw new Error(msg)
+// 	}
+// }
